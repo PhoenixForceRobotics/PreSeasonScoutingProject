c495fe2d2dae821bc2b31ccb951d4108309c33c3
Gabriel Adams <gja.code@gmail.com>
2022-10-11 17:17:54 -0700

    "ordered manifest"

---
+++ app/src/main/java/com/example/practiceappthingwearegoingtodo/Login.java
@@ -0,0 +1,127 @@
+package com.example.practiceappthingwearegoingtodo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import java.util.ArrayList;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Login extends AppCompatActivity {
+
+    int i;
+    EditText password;
+    Spinner users;
+    String user;
+    String enteredPass;
+    Button submit;
+    Boolean validated;
+    Boolean correct;
+
+    private List<String> usernames = new ArrayList<>();
+    private List<String> passwords = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        Context context = this;
+
+        users = findViewById(R.id.usernamesSpinner);
+        password = findViewById(R.id.editText);
+        submit = findViewById(R.id.button);
+
+        //creates passwords and users
+        addUsersPasswords();
+
+
+        submit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                validateUser();
+
+
+            }
+
+        });
+
+        usernames.add(0,"Select user");
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, usernames);
+        //set the spinners adapter to the previously created one.
+        users.setAdapter(adapter);
+    }
+
+
+    public void validateUser () {
+        correct = false;
+        i = 1;
+        while (correct == false) {
+
+
+            enteredPass = password.getText().toString();
+            user = users.getSelectedItem().toString();
+            //navigates either way also fix for
+            Log.d("passwords:", passwords.get(0));
+            Log.d("Username", usernames.get(0));
+            Log.d("EnteredUsername", user);
+            Log.d("EnteredPass", enteredPass);
+            //find entered user in database dont need to check if exists then
+
+
+            enteredPass = passwords.get(i);
+
+            //Integer.parse int somehow works -lord andrews fault
+            if (Integer.parseInt(enteredPass) == Integer.parseInt(String.valueOf(passwords.get(i)))) {
+                correct = true;
+                Log.d("made it here", user);
+                mainPage();
+
+
+            }
+            i = i + 1;
+        }
+
+    }
+    public void mainPage() {
+        Log.d("login", "saveuser ran");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+
+    }
+    public void addUsersPasswords()  {
+        Log.d("login", "added passwords and users ran");
+
+        usernames.add("Noam Sorlie");
+        usernames.add("Gabriel Adams");
+        usernames.add("Syed Ali");
+        usernames.add("Conner ???");
+        usernames.add("Malia ???");
+
+
+        passwords.add("6969");
+        passwords.add("6969");
+        passwords.add("6969");
+        passwords.add("6969");
+        passwords.add("6969");
+
+
+
+
+
+    }
+}
